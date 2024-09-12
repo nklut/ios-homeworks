308fba23d95616ea2c81f855fbb53197299b9b44
Nikita <nktlut@yandex.ru>
2024-09-12 11:14:14 +0800

    Tasks 1-4. Project setup. Task bar and Nav Controllers setup

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -13,10 +13,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        
+        let tabBarController = UITabBarController()
+           
+        let feedNavigationController = UINavigationController()
+        feedNavigationController.tabBarItem.title = "Feed"
+        feedNavigationController.tabBarItem.image = UIImage(systemName: "book.pages")
+        
+        let profileNavigationController = UINavigationController()
+        profileNavigationController.tabBarItem.title = "Profile"
+        profileNavigationController.tabBarItem.image = UIImage(systemName: "person")
+        
+        tabBarController.viewControllers = [feedNavigationController, profileNavigationController]
+           
+        window.rootViewController = tabBarController
+        window.makeKeyAndVisible()
+        self.window = window
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
