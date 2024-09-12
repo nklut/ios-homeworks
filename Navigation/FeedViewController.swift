ec8e1d4b773fee92db6acdff6b670f8c6edf22f0
Nikita <nktlut@yandex.ru>
2024-09-12 12:03:33 +0800

    Tasks 6-7. Button configuration. Post controller setup

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -1,35 +1,36 @@
-//
-//  FeedViewController.swift
-//  Navigation
-//
-//  Created by Nikita on 12.09.2024.
-//
-
 import UIKit
 
 class FeedViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .white
-        
-        self.view.addSubview(button)
-        self.button.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-        ])
-
-    }
     
+    // Button
     private var button: UIButton = {
         let button = UIButton(type: .roundedRect)
         button.setTitle("Open Post", for: .normal)
         return button
     }()
     
-    
-    
+    // Open Post Controller on press
+    @objc func didPressButton() {
+        let currentPost = Post(title: "Post_1")
+        let pvc = PostViewController(post: currentPost)
+        self.navigationController?.pushViewController(pvc, animated: true)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        // Add Button to Feed View
+        self.view.addSubview(button)
+        
+        // Button style and setup
+        self.button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+        self.button.addTarget(self, action: #selector(didPressButton), for: .touchUpInside)
+
+    }
 
 }
--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -1,20 +1,27 @@
-//
-//  PostViewController.swift
-//  Navigation
-//
-//  Created by Nikita on 12.09.2024.
-//
-
 import UIKit
 
-class PostViewController: UIViewController {
+struct Post {
+    let title: String
+}
 
+class PostViewController: UIViewController {
+    
+    var post: Post
+    
+    // Setup post init
+    init(post: Post) {
+        self.post = post
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = .systemBrown
-        self.title = "Post"
-        
+        self.title = post.title
     }
 
 }
