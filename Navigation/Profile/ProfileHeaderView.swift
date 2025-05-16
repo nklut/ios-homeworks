891ab67320d10d0942969356d4775d8b10bdc9ff
Nikita <nktlut@yandex.ru>
2025-05-16 17:31:30 +0800

    Profile picture and Profile name added

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -1,5 +1,47 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
+        
+    var profilePic: UIImageView
+    var profileName: UILabel
+    //var logo: UIView
+
+    
+    override init(frame: CGRect) {
+        
+        profileName = UILabel()
+        profilePic = UIImageView()
+        super.init(frame: frame)
+
+        // Profile picture seup
+        profilePic = UIImageView(image: UIImage(named: "cat"))
+        profilePic.frame = CGRect(x: 16.0, y: 16.0, width: 150, height: 150)
+        profilePic.layer.cornerRadius = 70.0
+        profilePic.layer.borderColor = UIColor.white.cgColor
+        profilePic.layer.borderWidth = 3
+        
+        // Profile Name
+        profileName.frame = CGRect(x: 200, y: 27, width: 100, height: 18)
+        profileName.text = "Hipster Cat"
+        profileName.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.bold)
+        profileName.textColor = .black
+        
+        
+        
+        profilePic.clipsToBounds = true
+       // profileName.layer.masksToBounds = true
+        
+        
+        
+        
+        addSubview(profilePic)
+        addSubview(profileName)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Not implemented")
+    }
     
 }
+
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -2,8 +2,51 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    private lazy var profileHeader: ProfileHeaderView = {
+        let view = ProfileHeaderView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .lightGray
+            
+        return view
+        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .lightGray
+        
+        title = "Profile"
+        self.view.backgroundColor = .systemBackground
+        view.addSubview(profileHeader)
+    
+        viewWillLayoutSubviews()
+        setupContraints()
+        
     }
+    
+    private func setupContraints() {
+        let safeAreaGuide = view.safeAreaLayoutGuide
+            
+        NSLayoutConstraint.activate([
+            profileHeader.leadingAnchor.constraint(
+                equalTo: safeAreaGuide.leadingAnchor,
+                constant: 0.0
+            ),
+            profileHeader.trailingAnchor.constraint(
+                equalTo: safeAreaGuide.trailingAnchor,
+                constant: 0.0
+            ),
+            profileHeader.topAnchor.constraint(
+                equalTo: safeAreaGuide.topAnchor,
+                constant: 0.0
+            ),
+            profileHeader.bottomAnchor.constraint(
+                equalTo: safeAreaGuide.bottomAnchor,
+                constant: 0.0
+            )
+        ])
+    }
+    
 }
+
+
+
+
